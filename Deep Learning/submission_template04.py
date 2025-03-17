9be91344f5d91f3f0df6deeff11a6e83e2f95e9e
Ярослав Егоров <yaroslav_egorov_1996@mail.ru>
2025-03-17 23:31:38 +0300

    Update submission_template04.py

--- Deep Learning/submission_template04.py
+++ Deep Learning/submission_template04.py
@@ -6,6 +6,10 @@ from torch.nn import functional as F
 class ConvNet(nn.Module):
     def __init__(self):
         super().__init__()
+
+        # ВАШ КОД ЗДЕСЬ
+        # определите слои сети
+
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=(5,5))
         self.pool1 = nn.MaxPool2d(kernel_size=(2,2))
         self.conv2 = nn.Conv2d(in_channels=3, out_channels=5, kernel_size=(3,3))
@@ -13,12 +17,15 @@ class ConvNet(nn.Module):
 
         self.flatten = nn.Flatten()
 
-        self.fc1 = nn.Linear(6*6*5, 100)
+        self.fc1 = nn.Linear(180,100)
         self.fc2 = nn.Linear(100, 10)
 
 
     def forward(self, x):
         # размерность х ~ [64, 3, 32, 32]
+
+        # ВАШ КОД ЗДЕСЬ
+        # реализуйте forward pass сети
 
         x = F.relu(self.conv1(x))
         x = self.pool1(x)
@@ -26,6 +33,7 @@ class ConvNet(nn.Module):
         x = self.pool2(x)
         
         x = self.flatten(x)
+        
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
